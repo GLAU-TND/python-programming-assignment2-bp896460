6415cafa37e325a5802983a00f815d6446238a47
bp896460 <60168087+bp896460@users.noreply.github.com>
2020-05-03 14:55:39 +0530

    Create Assignment_6_bottlenecks.py

---
+++ Assignment_6_bottlenecks.py
@@ -0,0 +1,17 @@
+'''
+Assignment 6
+Bottle Necks
+Problem
+
+'''
+def min_visible_bottles (arr,n):
+     a = dict()
+     ans = 0
+     for i in range (n):
+          a[arr[i]] = a.get(arr[i],0)+1
+          ans = max(ans,a[arr[i]])
+
+     print("Minimum number of",Visible Bottles are :",ans)
+n = int(input())
+arr = input().split()
+min_visible_bottles (arr,n)
